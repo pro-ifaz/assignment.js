a95b5f703561b6a9da6901720109cf32cbd7eb46
pro-ifaz <ifazahmed98@gmail.com>
2020-09-24 16:35:31 +0600

    feetToMile done

---
+++ feetToMile.js
@@ -0,0 +1,15 @@
+// creating a function to calculate feet to mile everytime when needed without repeating the same process.
+
+function feetToMile (feet){
+    var getMile = feet * 0.000189394;  // 1 feet = 0.000189394 mile [so its been multiplied]
+    return getMile;
+}
+
+// function is between the comment
+
+
+// calculation from feet to mile.
+
+var mileResult = feetToMile(12);
+
+console.log(mileResult);
