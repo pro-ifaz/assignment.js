35e3fedc112551dce8238e7f199bb680f632d29a
pro-ifaz <ifazahmed98@gmail.com>
2020-10-02 20:37:50 +0600

    correction done

--- feetToMile.js
+++ feetToMile.js
@@ -1,20 +1,9 @@
-// creating a function to calculate feet to mile everytime when needed without repeating the same process.
-
 function feetToMile(feet) {
 
-    var getMile = feet * 0.000189394;  // 1 feet = 0.000189394 mile [so its been multiplied]
-
-    // using an if case to prevent negative value insertion. Please Let me know if this ok.  
-
-    if (feet < -1) {
-        getMile = `negative values aren't allowed. calculation cancelled.`;
-    }
-
-    return getMile;
+    var mile = feet / 5280;
+    return mile;
 }
 
-// calculation 
-
-var result = feetToMile(-5280);
+var result = feetToMile(100000);
 
 console.log(result);
